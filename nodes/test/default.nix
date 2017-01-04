9fc95691143020ed7581f7271709d11a0da296fc
Stewart Mackenzie <setori88@gmail.com>
2017-01-05 00:15:00 +0800

    Problem: Schema in fractals will probably name collide
    Solution: schema must have full qualified class name and dir hierarchy

--- nodes/test/default.nix
+++ nodes/test/default.nix
@@ -2,16 +2,16 @@
 
 subgraph {
   src = ./.;
-  edges = with edges; [ net_http_edges.address generic_text ];
+  edges = with edges; [ net_http_edges.net_http_address prim_text ];
   flowscript = with nodes; with edges; ''
-  '${net_http_edges.address}:(address="127.0.0.1:8001")' -> listen controller(${controller})
-  '${generic_text}:(text="tcp://127.0.0.1:5551")' -> request_get controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5552")' -> request_post controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5553")' -> request_delete controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5554")' -> request_patch controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5555")' -> response_get controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5556")' -> response_post controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5557")' -> response_delete controller()
-  '${generic_text}:(text="tcp://127.0.0.1:5558")' -> response_patch controller()
+  '${net_http_edges.net_http_address}:(address="127.0.0.1:8001")' -> listen controller(${controller})
+  '${prim_text}:(text="tcp://127.0.0.1:5551")' -> request_get controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5552")' -> request_post controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5553")' -> request_delete controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5554")' -> request_patch controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5555")' -> response_get controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5556")' -> response_post controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5557")' -> response_delete controller()
+  '${prim_text}:(text="tcp://127.0.0.1:5558")' -> response_patch controller()
   '';
 }
