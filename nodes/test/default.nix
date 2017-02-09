2f80bfe9cb86e5a8818b73a9a67fa99146301710
Stewart Mackenzie <setori88@gmail.com>
2017-02-09 21:31:33 +0800

    Problem: capnp compilation process must be fbp agnostic
    Solution: the product of capnp encode should be the imsg
    input. capnp tool shouldn't be called from rust code because
    that means the fbp graph implementation is dependent on capnp at
    run time.
    
    By making the fbp graph implementation dependent on the product
    of the capnp encode output the graph becomes portable. i.e.
    subgraphs can be parsed in javascript and the implemantation
    doesn't have to depend on capnp tool at runtime... in the
    browser this is problematic.

--- nodes/test/default.nix
+++ nodes/test/default.nix
@@ -1,19 +1,26 @@
-{ subgraph, imsgs, nodes, edges }:
-
-subgraph rec {
+{ subgraph, imsg, nodes, edges }:
+let
+  NetHttpAddress = imsg { class = edges.NetHttpEdges.NetHttpAddress; text = ''(address="127.0.0.1:8001")''; };
+  PrimText1 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5551")''; };
+  PrimText2 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5552")''; };
+  PrimText3 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5553")''; };
+  PrimText4 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5554")''; };
+  PrimText5 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5555")''; };
+  PrimText6 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5556")''; };
+  PrimText7 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5557")''; };
+  PrimText8 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5558")''; };
+in
+subgraph {
   src = ./.;
-  imsg = imsgs {
-    edges = with edges; [NetHttpEdges.NetHttpAddress PrimText];
-  };
   flowscript = with nodes; ''
-  '${imsg}.NetHttpAddress:(address="127.0.0.1:8001")' -> listen controller(${controller})
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5551")' -> request_get controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5552")' -> request_post controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5553")' -> request_delete controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5554")' -> request_patch controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5555")' -> response_get controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5556")' -> response_post controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5557")' -> response_delete controller()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5558")' -> response_patch controller()
+  '${NetHttpAddress}' -> listen controller(${controller})
+  '${PrimText1}' -> request_get controller()
+  '${PrimText2}' -> request_post controller()
+  '${PrimText3}' -> request_delete controller()
+  '${PrimText4}' -> request_patch controller()
+  '${PrimText5}' -> response_get controller()
+  '${PrimText6}' -> response_post controller()
+  '${PrimText7}' -> response_delete controller()
+  '${PrimText8}' -> response_patch controller()
   '';
 }
